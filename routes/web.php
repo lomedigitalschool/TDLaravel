488108f4f4efe3de111f695bc8e801bef175c56d
samwin25 <edorhsam@gmail.com>
2025-06-09 09:34:58 +0000

    TEST17

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\RecetteController;
 |--------------------------------------------------------------------------
 */
 Route::get('/', function () {
-    return 'Laravel fonctionne !';
+    return view('welcome');
 })->name('home'); // Ajout d'un nom pour faciliter les redirections
 
 /*
